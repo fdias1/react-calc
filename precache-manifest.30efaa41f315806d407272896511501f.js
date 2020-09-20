9156870fca5c4157510eeef05e42584883071c43
Fábio Dias <fabbiodiaz@gmail.com>
2020-09-19 22:41:11 -0300

    Updates

---
+++ precache-manifest.30efaa41f315806d407272896511501f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51200460074a896f5c73912de7d320bf",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "3b795869b9dcaebaf06c",
+    "url": "/react-calc/static/css/main.0f164707.chunk.css"
+  },
+  {
+    "revision": "e1c5668b6e090dd33f0d",
+    "url": "/react-calc/static/js/2.9546e2ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.9546e2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b795869b9dcaebaf06c",
+    "url": "/react-calc/static/js/main.dd2c6e71.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
